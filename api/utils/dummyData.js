19eb01302744203fac54286738ae2eaa64b874cd
Frankabonyi <frankabonyi@gmail.com>
2019-10-25 13:20:02 -0700

    create feature meal service

---
+++ api/utils/dummyData.js
@@ -0,0 +1,32 @@
+export default {
+
+    meals: [
+
+        {
+            id: 1,
+            name: 'Fried Rice',
+            size: 'Medium',
+            prize: '500'
+        },
+        {
+            id: 2,
+            name: 'Jellof Rice',
+            size: 'Large',
+            prize: '550'
+        },
+        {
+            id: 3,
+            name: 'Achicha',
+            size: 'Medium',
+            prize: '300'
+        },
+        {
+            id: 4,
+            name: 'Eba & Egwusi',
+            size: 'Medium',
+            prize: '350'
+        }       
+
+    ]
+
+}
